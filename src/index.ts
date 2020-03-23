841a3d75a4c435d8ebd4c4b3ac2b4eff42431c8d
Roman Filippov <rf@romanfilippov.com>
2020-03-23 19:35:45 +0700

    style: fixes linting issues after pretty update to 2

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ const pkgRenovateConfig = pkg['renovate-config'] as Record<string, unknown>
 
 Object.keys(presets)
   .sort()
-  .forEach(key => (pkgRenovateConfig[key] = presets[key]))
+  .forEach((key) => (pkgRenovateConfig[key] = presets[key]))
 
 writeFileSync(join(__dirname, '../package.json'), JSON.stringify(pkg, null, 2), { encoding: 'utf8' })
